411a21300478aad27d3245437e4dff522217bb53
Evadne Wu <ev@radi.ws>
2012-12-06 21:38:55 -0800

    AHEasing -> AHEasing-Sample; adds AHEasing.a (libAHEasing)

---
+++ AHEasing/AHEasing.h
@@ -0,0 +1,10 @@
+//
+//  AHEasing.h
+//  AHEasing
+//
+//  Created by Evadne Wu on 12/6/12.
+//  Copyright (c) 2012 Auerhaus Development, LLC. All rights reserved.
+//
+
+#import "easing.h"
+#import "CAKeyframeAnimation+AHEasing.h"
